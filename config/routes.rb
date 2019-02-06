9c64a3729f1b3a4b77d19224598d621c9b56ed9d
Mica Delmonte <eladelmonte@gmail.com>
2019-02-06 17:39:33 +0900

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # DEFAULT HOME PAGE 
+  root 'posts#index'
+
+  # LOG IN MODULE
+  get 'login' => 'pages#login'
+
+  resources :posts
+  
+end
