f521dcd82424570792bc2a4fb42cba5e6adf2a53
BIT-Okutama <BITokutama@gmail.com>
2019-02-08 18:03:58 +0900

    Backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,11 @@
 Rails.application.routes.draw do
+	
+  # ROOT FOR USERS: LOG IN LOG OUT SIGN UP
   devise_for :users
-  resources :contents
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
 
-  # DEFAULT HOME PAGE 
+  # BOARD PAGE
   root 'contents#index'
   resources :contents
 
-  # SIGN UP ROUTE
-  #get  '/signup',  to: 'users#new'
-  #post '/signup',  to: 'users#create'
-
-  # LOG IN ROUTE
-  #get    '/login',   to: 'sessions#new'
-  #post   '/login',   to: 'sessions#create'
-  #delete '/logout',  to: 'sessions#destroy'
-
-  resources :users
-  
-
-  # CONTENT
-  #get    '/board', to: 'contents#index'
-  #resources :contents
 end
